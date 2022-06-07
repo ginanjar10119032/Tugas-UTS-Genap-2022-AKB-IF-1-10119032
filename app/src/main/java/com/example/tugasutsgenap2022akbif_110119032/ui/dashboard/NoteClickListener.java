52bcc60704ee75d4959af49ab3125c3cd25cca51
ginanjartg <ginanjar.10119032@mahasiswa.unikom.ac.id>
2022-06-07 19:43:05 +0700

    FINAL COMMIT
    sudah beres

--- app/src/main/java/com/example/tugasutsgenap2022akbif_110119032/ui/dashboard/NoteClickListener.java
+++ app/src/main/java/com/example/tugasutsgenap2022akbif_110119032/ui/dashboard/NoteClickListener.java
@@ -1,5 +1,9 @@
 package com.example.tugasutsgenap2022akbif_110119032.ui.dashboard;
 
+//NIM     : 10119032
+//NAMA    : GINANJAR TUBAGUS GUMILAR
+//KELAS   : IF-1
+
 public interface NoteClickListener {
     void onNoteClicked(int rowId, String subject, String description);
 }
--- app/src/main/java/com/example/tugasutsgenap2022akbif_110119032/ui/dashboard/NotesManager.java
+++ app/src/main/java/com/example/tugasutsgenap2022akbif_110119032/ui/dashboard/NotesManager.java
@@ -1,5 +1,9 @@
 package com.example.tugasutsgenap2022akbif_110119032.ui.dashboard;
 
+//NIM     : 10119032
+//NAMA    : GINANJAR TUBAGUS GUMILAR
+//KELAS   : IF-1
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
